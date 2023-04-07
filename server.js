588550895a1337b7ae97df7c5f916935bdd9c00f
Pranjal Kamboj <pranjalkamboj007@gmail.com>
2023-04-07 12:00:45 +0530

    404 page

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.use(bodyParser.urlencoded({extended : false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// 404 page middleware
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+  });
+
+  
 app.listen(3000, () => {
     console.log("Server is listening on PORT 3000")
 })
